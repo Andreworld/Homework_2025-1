8fe7573d3396d5d515ac58d6f4286e5527e57055
Andreworld <palvinskiy@gmail.com>
2025-07-09 16:58:45 +0400

    Добавил в папку JS

---
+++ script.js
@@ -0,0 +1,67 @@
+// 1.
+let a = 10;
+alert (a);
+a = 20;
+alert (a);
+
+// 2.
+
+const iPhone1 = 2007;
+alert (iPhone1);
+
+// 3.
+
+const JavaScript = "Брендан Эйх";
+alert (JavaScript);
+
+// 4.
+
+let c = 10;
+let d = 2;
+alert (c + d);
+alert (c - d);
+alert (c * d);
+alert (c / d);
+alert (c % d);
+
+// 5.
+
+alert (2 ** 5);
+
+// 6.
+
+a = 9;
+b = 2;
+alert (a % b);
+
+// 7.
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// 8.
+
+let age = prompt("Сколько вам лет?");
+alert (age);
+
+// 9.
+
+const user = {
+    name: "Прокофий",
+    age: 99,
+    isAdmin: false
+};
+let info = prompt("Что вы хотите узнать о пользователе?");
+console.log(user[info]);
+
+// 10.
+
+let name = prompt ("Как ваше имя?");
+alert(`Приветствую тебя, ${name}!`);
+
